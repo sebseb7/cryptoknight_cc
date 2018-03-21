4eb88e5559f35409157483acedd25aa9de20e7fa
sebseb7 <github@seb.exse.net>
2018-03-21 05:45:20 +0100

    patcher

---
+++ apply_patch.sh
@@ -0,0 +1,82 @@
+cd alloy
+cat ../patch | patch -p1
+cd ..
+cd b2b
+cat ../patch | patch -p1
+cd ..
+cd bbs
+cat ../patch | patch -p1
+cd ..
+cd btcn
+cat ../patch | patch -p1
+cd ..
+cd crep
+cat ../patch | patch -p1
+cd ..
+cd dero
+cat ../patch | patch -p1
+cd ..
+cd dinasty
+cat ../patch | patch -p1
+cd ..
+cd edl
+cat ../patch | patch -p1
+cd ..
+cd elya
+cat ../patch | patch -p1
+cd ..
+cd lines
+cat ../patch | patch -p1
+cd ..
+cd fcb
+cat ../patch | patch -p1
+cd ..
+cd graft
+cat ../patch | patch -p1
+cd ..
+cd haven
+cat ../patch | patch -p1
+cd ..
+cd ipbc
+cat ../patch | patch -p1
+cd ..
+cd itns
+cat ../patch | patch -p1
+cd ..
+cd karbo
+cat ../patch | patch -p1
+cd ..
+cd msr
+cat ../patch | patch -p1
+cd ..
+cd niobio
+cat ../patch | patch -p1
+cd ..
+cd ombre
+cat ../patch | patch -p1
+cd ..
+cd qwerty
+cat ../patch | patch -p1
+cd ..
+cd royal
+cat ../patch | patch -p1
+cd ..
+cd stellite
+cat ../patch | patch -p1
+cd ..
+cd triton
+cat ../patch | patch -p1
+cd ..
+cd truck
+cat ../patch | patch -p1
+cd ..
+cd turtle
+cat ../patch | patch -p1
+cd ..
+cd v7
+cat ../patch | patch -p1
+cd ..
+cd xun
+cat ../patch | patch -p1
+cd ..
+
