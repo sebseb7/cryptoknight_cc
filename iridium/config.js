72fc199954d43773d05291c92fd6c90d9e912866
sebseb7 <sebgreenbus@gmail.com>
2018-03-26 07:21:13 +0200

    iridium

---
+++ iridium/config.js
@@ -0,0 +1,18 @@
+var api = "/rpc/iridium";
+
+var poolHost = "94.130.207.37";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "https://explorer.ird.cash/?hash={id}#blockchain_block";
+var transactionExplorer = "https://explorer.ird.cash/?hash={id}#blockchain_transaction";
+
+var guiWallet = "http://ird.cash/";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
