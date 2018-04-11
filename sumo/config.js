a8ed612a0a1da7070a7587a7509d976e34552951
sebseb7 <sebgreenbus@gmail.com>
2018-04-11 23:25:25 +0200

    sumo

---
+++ sumo/config.js
@@ -0,0 +1,20 @@
+var api = "/rpc/sumo";
+
+var poolHost = "sumo.ingest.cryptoknight.cc";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "https://explorer.sumokoin.com/block/{id}";
+var transactionExplorer = "https://explorer.sumokoin.com/tx/{id}";
+
+var guiWallet = "https://www.sumokoin.org/";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+
