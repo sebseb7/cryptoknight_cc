d4663d479e37ee546b0e37dd5ce3d3f5d9a1c737
sebseb7 <sebgreenbus@gmail.com>
2018-04-11 23:24:58 +0200

    italo

---
+++ italo/config.js
@@ -0,0 +1,18 @@
+var api = "/rpc/italo";
+
+var poolHost = "italo.ingest.cryptoknight.cc";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "https://explorer.italocoin.com/block/{id}";
+var transactionExplorer = "https://explorer.italocoin.com/tx/{id}";
+
+var guiWallet = "https://www.italocoin.com/page/downloads";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
