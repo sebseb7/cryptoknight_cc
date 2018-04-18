d4ea947f950ff3c1d18d44947e9fd5d0f48b781b
sebseb7 <sebgreenbus@gmail.com>
2018-04-18 03:39:56 +0200

    misc

---
+++ apply_patch2.sh
@@ -0,0 +1,15 @@
+cd lines
+cat ../patch2 | patch -p1
+cd ..
+cd iridium
+cat ../patch2 | patch -p1
+cd ..
+cd italo
+cat ../patch2 | patch -p1
+cd ..
+cd sumo
+cat ../patch2 | patch -p1
+cd ..
+cd stellite
+cat ../patch2 | patch -p1
+cd ..
