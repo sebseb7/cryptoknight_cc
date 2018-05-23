5a33251b3a34b4d1821b59d24644280ff58dc06d
sebseb7 <sebgreenbus@gmail.com>
2018-05-23 14:30:40 +0200

    arto

---
+++ arto/config.js
@@ -0,0 +1,18 @@
+var api = "/rpc/arto";
+
+var poolHost = "arto.ingest.cryptoknight.cc";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_transaction";
+
+var guiWallet = "https://github.com/artocash/artowallet/releases";
+
+var paperWallet = "";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
