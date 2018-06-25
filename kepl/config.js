7556303534d554167216c5af26272f094b645c49
sebseb7 <sebgreenbus@gmail.com>
2018-06-26 01:03:21 +0200

    kepl

---
+++ kepl/config.js
@@ -0,0 +1,21 @@
+var api = "/rpc/kepl";
+
+var poolHost = "kepl.ingest.cryptoknight.cc";
+
+var cryptonatorWidget = ["BTC-USD"];
+
+var irc = "";
+
+var email = "";
+
+var blockchainExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_block";
+var transactionExplorer = "http://explorer.kepl.org/?hash={id}#blockchain_transaction";
+
+var guiWallet = "http://www.kepl.org/";
+
+var paperWallet = "";
+//var paperWallet = "pages/paperwallet.html";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
+
+
