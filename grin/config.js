e532c45e98ff6b715f416ed70dd90e29f51b8093
sebseb7 <sebgreenbus@gmail.com>
2019-02-17 11:58:26 +0100

    swap & grin

---
+++ grin/config.js
@@ -0,0 +1,13 @@
+var api = "/rpc/grin";
+
+var email = "";
+var telegram = "";
+var discord = "";
+
+var marketCurrencies = [];
+
+var blockchainExplorer = "https://grinscan.net/block/{id}";
+var transactionExplorer = "";
+
+var themeCss = "themes/moon_gray_dark.css";
+var defaultLang = 'en';
